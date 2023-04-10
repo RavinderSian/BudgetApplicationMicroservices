268f5adbbe984d40794143ac0440fe155497eb5b
ravinder sian <rsian761@gmail.com>
2023-04-10 16:24:45 +0100

    add expense list changes

--- budget/budget-react/src/main/webapp/src/Components/Expense/ExpenseList.js
+++ budget/budget-react/src/main/webapp/src/Components/Expense/ExpenseList.js
@@ -1,31 +1,55 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Expense from "./Expense";
 import ExpenseTotal from "./ExpenseTotal";
 import classes from "./ExpenseList.module.css";
 
-const DUMMY_EXPENSE = [
-  {
-    id: 1,
-    category: "TEST",
-    description: "test",
-    amount: 50.0,
-    purchaseDate: "2023-01-02",
-  },
-  {
-    id: 2,
-    category: "TEST",
-    description: "test",
-    amount: 60.0,
-    purchaseDate: "2023-01-02",
-  },
-];
+// const DUMMY_EXPENSE = [
+//   {
+//     id: 1,
+//     category: "TEST",
+//     description: "test",
+//     amount: 50.0,
+//     purchaseDate: "2023-01-02",
+//   },
+//   {
+//     id: 2,
+//     category: "TEST",
+//     description: "test",
+//     amount: 60.0,
+//     purchaseDate: "2023-01-02",
+//   },
+// ];
 
 const ExpenseList = () => {
-  const totalOfExpenses = DUMMY_EXPENSE.reduce((a, b) => {
+  const fetchExpenses = useCallback(async function () {
+    const response = await fetch("http://localhost:8080/expenses/rsian", {
+      mode: "cors",
+    });
+    const data = await response.json();
+    const transformedExpenses = data.map((expenseData) => {
+      return {
+        id: expenseData.id,
+        userId: expenseData.userId,
+        category: expenseData.category,
+        amount: expenseData.amount,
+        description: expenseData.description,
+        purchaseDate: expenseData.purchaseDate,
+      };
+    });
+    setExpensesListFromDB(transformedExpenses);
+    console.log(transformedExpenses);
+  }, []);
+
+  const [expensesListFromDB, setExpensesListFromDB] = useState([]);
+
+  const totalOfExpenses = expensesListFromDB.reduce((a, b) => {
     return a + b.amount;
   }, 0);
 
-  const expensesList = DUMMY_EXPENSE.map((expense) => (
+  useEffect(() => {
+    fetchExpenses();
+  }, [fetchExpenses]);
+  const expensesList = expensesListFromDB.map((expense) => (
     <Expense key={expense.id} {...expense}></Expense>
   ));
   return (
@@ -35,5 +59,4 @@ const ExpenseList = () => {
     </React.Fragment>
   );
 };
-
 export default ExpenseList;
